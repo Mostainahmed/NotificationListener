010674b545a02e3d4a5bddac339f1f7618811ab9
Mostainahmed <mostainahmed@gmail.com>
2018-10-06 05:58:44 +0600

    First Commit

---
+++ app/src/main/java/android/notifications/Model.java
@@ -0,0 +1,27 @@
+package android.notifications;
+
+import android.graphics.Bitmap;
+
+/**
+ * Created by mostain on 28/7/18.
+ */
+public class Model {
+    String name;
+    Bitmap imaBitmap;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Bitmap getImage() {
+        return imaBitmap;
+    }
+
+    public void setImage(Bitmap imaBitmap) {
+        this.imaBitmap = imaBitmap;
+    }
+}
